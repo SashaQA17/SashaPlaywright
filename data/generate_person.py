d2e71111303aa6ec7fd6ccea7e4d98788c7a524a
sasha <halushkevich.a17@gmail.com>
2024-02-20 02:29:12 +0400

    add new pages and tests

---
+++ data/generate_person.py
@@ -0,0 +1,11 @@
+from data.data_person import Person
+from faker import Faker
+
+faker_en = Faker('En')
+
+def generate():
+    return Person(
+        first_name=faker_en.first_name(),
+        last_name=faker_en.last_name(),
+        email=faker_en.email()
+    )
---
+++ pages/left_menu_page.py
@@ -0,0 +1,61 @@
+from pages.base_page import BasePage
+import allure
+
+
+class LeftMenuPage(BasePage):
+
+    book_store_application_btn = '//*[text()="Book Store Application"]'
+    login_bnt = '//*[text() = "Login"]'
+    book_store_btn = '//*[text() = "Book Store"]'
+    book_profile_btn = '//*[text() = "Profile"]'
+    book_book_store_api_btn = '//*[text() = "Book Store API"]'
+    practice_form_btn = '//span[text()="Practice Form"]'
+    practice_form_btn_text = 'Practice Form'
+
+    def __init__(self, page):
+        super().__init__(page)
+
+#click
+    def click_practice_form_btn(self):
+        with allure.step("click practice form btn"):
+            self.click_by_text(self.practice_form_btn_text)
+            return self
+
+    def click_book_store_application_btn(self):
+        with allure.step("click book store application btn"):
+            self.click_by_locator(self.book_store_application_btn)
+            return self
+
+# check
+    def check_practice_form_btn(self, locator: tuple):
+        with allure.step("check practice form btn"):
+            self.check_elm(locator)
+            return self
+
+    def check_login_btn(self, locator):
+        with allure.step("check login btn"):
+            self.check_elm(locator)
+            return self
+
+    def check_book_store_btn(self, locator):
+        with allure.step("check book store btn"):
+            self.check_elm(locator)
+            return self
+
+    def check_profile_btn(self, locator):
+        with allure.step("check profile btn"):
+            self.check_elm(locator)
+            return self
+
+    def check_book_store_api_btn(self, locator):
+        with allure.step("check_book_store_api_btn"):
+            self.check_elm(locator)
+            return self
+
+
+
+
+
+
+
+
